c288df330e691d50019df269f9cd5754650562ea
Ramon Hernandez <eldiaadia24h@gmail.com>
2025-01-21 17:01:35 -0300

    Mejora tarjeta seccion futbol

--- src/data/jugadores.ts
+++ src/data/jugadores.ts
@@ -7,7 +7,7 @@ export interface Jugador {
   estilo: string;
   imagen: string;
   equipoFavorito: {
-    nombre: "Real Madrid" | "Barcelona" | "Manchester City" | "PSG";
+    nombre: string;
     colores: {
       primario: string;
       secundario: string;
@@ -30,14 +30,14 @@ export const jugadores: Jugador[] = [
     nombre: "Robert ",
     apodo: "El nazi",
     skills: {
-      velocidad: 90,
-      tiro: 12,
-      pase: 12,
-      defensa: 10
+      velocidad: 78,
+      tiro: 70,
+      pase: 82,
+      defensa: 85
     },
     edad: 35,
     posicion: "Mediocampista",
-    numero: 10,
+    numero: 8,
     equipo: "Brosky FC",
     estilo: "Técnico",
     imagen: "/images/robert-fut.jpeg",
@@ -47,21 +47,69 @@ export const jugadores: Jugador[] = [
         primario: "#FFFFFF",
         secundario: "#00529F"
       },
+      escudo: "/images/realmadrid.png"
+    }
+  },
+  {
+    nombre: "Angel",
+    apodo: "Tiki Taka",
+    skills: {
+      velocidad: 86,
+      tiro: 78,
+      pase: 69,
+      defensa: 85
+    },
+    edad: 32,
+    posicion: "Mediocampista",
+    numero: 10,
+    equipo: "Brosky FC",
+    estilo: "Técnico",
+    imagen: "/images/jugadores/angel.jpeg",
+    equipoFavorito: {
+      nombre: "Barcelona",
+      colores: {
+        primario: "#004D98",
+        secundario: "#A50044"
+      },
       escudo: "/images/barsa.png"
     }
   },
   {
-    nombre: "Ramon Hernandez",
+    nombre: "Manuel",
+    apodo: "El portugues",
+    skills: {
+      velocidad: 60,
+      tiro: 86,
+      pase: 65,
+      defensa: 48
+    },
+    edad: 32,
+    posicion: "Delantero",
+    numero: 7,
+    equipo: "Brosky FC",
+    estilo: "Táctico",
+    imagen: "/images/jugadores/manuel.jpeg",
+    equipoFavorito: {
+      nombre: "Real Madrid",
+      colores: {
+        primario: "#FFFFFF",
+        secundario: "#00529F"
+      },
+      escudo: "/images/realmadrid.png"
+    }
+  },
+  {
+    nombre: "Ramon",
     apodo: "Parte Rodilla",
     skills: {
       velocidad: 90,
-      tiro: 80,
-      pase: 80,
-      defensa: 5
+      tiro: 66,
+      pase: 77,
+      defensa: 60
     },
     edad: 25,
     posicion: "Delantero",
-    numero: 666,
+    numero: 6,
     equipo: "Portales FC",
     estilo: "Goleador",
     imagen: "/images/jugadores/ramon.jpeg",
@@ -72,6 +120,79 @@ export const jugadores: Jugador[] = [
         secundario: "#A50044"
       },
       escudo: "/images/barsa.png"
+    }
+  },
+
+  {
+    nombre: "Andres",
+    apodo: "El analista",
+    skills: {
+      velocidad: 60,
+      tiro: 72,
+      pase: 60,
+      defensa: 86
+    },
+    edad: 28,
+    posicion: "Defensa",
+    numero: 3,
+    equipo: "Portales FC",
+    estilo: "Estratega",
+    imagen: "/images/jugadores/andres1.jpeg",
+    equipoFavorito: {
+      nombre: "Barcelona",
+      colores: {
+        primario: "#004D98",
+        secundario: "#A50044"
+      },
+      escudo: "/images/barsa.png"
+    }
+  },
+  {
+    nombre: "Axel",
+    apodo: "Axelito",
+    skills: {
+      velocidad: 60,
+      tiro: 72,
+      pase: 60,
+      defensa: 86
+    },
+    edad: 23,
+    posicion: "Mediocampista",
+    numero: 3,
+    equipo: "Portales FC",
+    estilo: "Estratega",
+    imagen: "/images/jugadores/axel.PNG",
+    equipoFavorito: {
+      nombre: "Vinotinto",
+      colores: {
+        primario: "#722F37",
+        secundario: "#000000"
+      },
+      escudo: "/images/vinotinto.jpeg"
+    }
+  },
+  {
+    nombre: "Darwin",
+    apodo: "Chencho",
+    skills: {
+      velocidad: 76,
+      tiro: 74,
+      pase: 87,
+      defensa: 82
+    },
+    edad: 20,
+    posicion: "Mediocampista",
+    numero: 21,
+    equipo: "Brosky FC",
+    estilo: "Lider",
+    imagen: "/images/jugadores/darwin.PNG",
+    equipoFavorito: {
+      nombre: "Vinotinto",
+      colores: {
+        primario: "#722F37",
+        secundario: "#000000"
+      },
+      escudo: "/images/vinotinto.jpeg"
     }
   }
 ]
